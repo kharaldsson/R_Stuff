e7cac9cba08d65068fed5b3b1a015940cc3c6963
Karl Haraldsson <karl.haraldsson@gmail.com>
2016-02-01 16:15:38 -0600

    Update twitteR_API_Connector.R

--- twitteR_API_Connector.R
+++ twitteR_API_Connector.R
@@ -17,3 +17,20 @@ twitter_token <- oauth1.0_token(oauth_endpoints("twitter"), myapp)
 req <- GET("https://api.twitter.com/1.1/statuses/home_timeline.json",config(token = twitter_token))
 stop_for_status(req)
 content(req)
+
+
+#can also use this method
+#clear 
+rm(list=ls())
+
+library(twitteR)
+library(ROAuth)
+library(RCurl)
+
+options(httr_oauth_cache=T)
+api_key <- "REMOVETHISCAPITALIZEDSTRINGIGgvbQIyj9kzEOxJ3QnwHUP7A"
+api_secret <- "REMOVETHISCAPITALIZEDSTRINGnRR5r3svua7RGUwseGuKeJFyoo2I2MADPSRogQHEjd94ZIBoff"
+access_token <- "REMOVETHISCAPITALIZEDSTRING1965566857-JP80nKuold7m1q1IA4K5iRve2ej1uwDuZQ6EZiw"
+access_token_secret <- "REMOVETHISCAPITALIZEDSTRINGEO2crNYwATZESKwQptLCOeGEGyWESLzH0ZlIZfocp4xj9"
+
+setup_twitter_oauth(api_key,api_secret)
